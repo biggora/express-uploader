ad2afd6cde033a9ed3d2c4813bbe0e6efe1dd06e
Alex <aleksej@gordejev.lv>
2014-08-17 14:44:33 +0300

    Fix  example

--- examples/server.js
+++ examples/server.js
@@ -22,38 +22,56 @@
  * THE SOFTWARE.
  */
 
-var express = require('express'),
-        Uploader = require('../lib/express-uploader'),
-        app = express();
+var fs = require('fs');
+var express = require('express');
+var multiparty = require('connect-multiparty');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var cookieParser = require('cookie-parser');
+var morgan = require('morgan');
+var Uploader = require('../lib/express-uploader');
+var app = express();
 
 // Configuration
-app.configure(function() {
-    app.use(express.static(__dirname + '/public'));
-    app.use(express.logger('dev'));
-    // We need the bodyParser to form parsing old style uploads
-    app.use(express.bodyParser({
-      //  uploadDir: './tmpUploads',
-        keepExtensions: true,
-        encoding: 'utf8'
-    }));
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(express['static'](__dirname));
-});
 
-// Create tmp dir
-// fs.mkdirSync('./tmpUploads', 755);
+app.use(morgan('dev'));
+// We need the bodyParser to form parsing old style uploads
+app.use(multiparty({
+    uploadDir: __dirname + '/tmp',
+    keepExtensions: true,
+    encoding: 'utf8'
+}));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+// parse application/json
+app.use(bodyParser.json());
+// parse application/vnd.api+json as json
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(methodOverride('X-HTTP-Method'));              // Microsoft
+app.use(methodOverride('X-HTTP-Method-Override'));     // Google/GData
+app.use(methodOverride('X-Method-Override'));          // IBM
+app.use(methodOverride('_method')); 	           // simulate DELETE and PUT
+app.use(methodOverride(function(req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        var method = req.body._method;
+        delete req.body._method;
+        return method;
+    }
+}));
+app.use(cookieParser('weritas10'));
+
 
 /*
  * Display upload form
  */
-app.all('/',function (req, res) {
+app.all('/', function(req, res) {
     res.send(
-        '<form action="/upload" method="post" enctype="multipart/form-data">'+
-        '<input type="file" name="upload-file"  multiple="true">'+
-        '<input type="submit" value="Upload">'+
-        '</form>'
-    );
+            '<form action="/upload" method="post" enctype="multipart/form-data">' +
+            '<input type="file" name="upload-file"  multiple="true">' +
+            '<input type="submit" value="Upload">' +
+            '</form>'
+            );
 });
 
 /*
@@ -69,13 +87,18 @@ app.all('/upload', function(req, res, next) {
         publicDir: __dirname + '/public',
         uploadDir: __dirname + '/public/files',
         uploadUrl: '/files/',
-        thumbSizes: [140,[100, 100]]
+        thumbSizes: [140, [100, 100]]
     });
 
     uploader.uploadFile(req, function(data) {
         res.send(JSON.stringify(data), {'Content-Type': 'text/plain'}, 200);
     });
 });
+
+// Create tmp dir
+if (!fs.existsSync(__dirname + '/tmp')) {
+    fs.mkdirSync(__dirname + '/tmp', 0755);
+}
 
 app.listen(3000, '127.0.0.1');
 console.log("Express server listening on %s:%d for uploads", '127.0.0.1', 3000);
